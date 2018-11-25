c1be5b9326306bbaf61855de02e661812472daf4
Leroy Merlos <leroymerlos@gmail.com>
2018-11-25 12:30:05 +0100

    Ejercicio 1

--- src/PaisEuropeo.java
+++ src/PaisEuropeo.java
@@ -53,10 +53,5 @@ public class PaisEuropeo {
 		
 		return categoria;
 	}
-	
-	// Ejercicio 2
-	public double mediaPoblacion(PaisEuropeo pais1, PaisEuropeo pais2){
-		return (pais1.poblacion + pais2.poblacion) / 2;
-	}
 
 }
