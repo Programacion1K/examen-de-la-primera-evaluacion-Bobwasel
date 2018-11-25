11dfce5379cbdc805499f9ed077bbaa9ec2e9f39
Leroy Merlos <leroymerlos@gmail.com>
2018-11-25 12:48:11 +0100

    Test

---
+++ src/Test.java
@@ -0,0 +1,39 @@
+public class Test {
+	public static void main (String[] args)
+	{
+		/*
+		 * 1. ¿Cómo se crea una instancia de la clase Person?
+		 * 		a) Person p = Person() 
+		 *		b) Class Person = new Person() 
+		 *		c) Person = new Person() 
+		 *		d) Person p = new Person() --> CORRECTA
+		 * 2. ¿Una última línea legal de un método declarado como public static int getVal(double sum) sería?
+		 *		a) return; 
+		 *		b) return 77; --> CORRECTA
+		 *		c) return 2.3; 
+		 *		d) cualquiera de las anteriores 
+		 * 3. ¿Qué valor almacenará tricky evaluando la siguiente expresión? 				
+         *      boolean tricky = (5<=5) == true;
+		 *		a) 5 
+		 *		b) false 
+		 *		c) true --> CORRECTA
+		 *		d) error 
+		 *	4. ¿Cuál no es un operador lógico?
+		 *		a) && 
+		 *		b) ? --> CORRECTA
+		 *		c) ! 
+		 *		d) || 
+		 *	5. ¿Qué significa sobrecargar (overload) un método?
+		 *		a) Editarlo para modificar su comportamiento
+		 *		b) Crear un método con el mismo nombre pero otros argumentos --> CORRECTA
+		 *		c) Cambiarle el nombre dejándolo con la misma funcionalidad
+		 *		d) Añadirle funcionalidades a un método
+		 * 6. ¿Por qué los métodos de la clase Math se pueden usar sin usar el operador new previamente?
+		 *		a) Porque siempre existe un objeto de esta clase 
+		 *		b) Porque Java crea el objeto por defecto 
+		 *		c) Porque sus métodos son estáticos --> CORRECTA
+		 *		d) No es cierto, siempre hay que crear un objeto con new 
+		 *
+		 */
+	}
+}
